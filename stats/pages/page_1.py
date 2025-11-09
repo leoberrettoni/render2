4471c187333eed420a6c0fd0bcb5d30bd1e937d1
leoberrettoni <127773205+leoberrettoni@users.noreply.github.com>
2025-11-09 23:14:02 +0100

    Refactor page_1.py for improved readability

--- stats/pages/page_1.py
+++ stats/pages/page_1.py
@@ -1,42 +1,18 @@
-'''
-esegui lo script python3 grafici.py e poi vai su http://127.0.0.1:8052/
-'''
-
-
-
-
-
-
-'''
-esegui lo script python3 grafici.py e poi vai su http://127.0.0.1:8052/
-'''
-
-
-
-
-
-
-import dash
-from dash import dcc, html, Input, Output, callback
-import dash_bootstrap_components as dbc
-import plotly.graph_objs as go
 import plotly.express as px
 from dash import dash_table
 import pandas as pd
+from pathlib import Path
 
 # connecting the page to the app.py file
 dash.register_page(__name__, path='/page-1', name='Statistiche per partita')
 
-# === Dataset ===
-'''df_luiss_giocatori = pd.read_csv(
-    "/Users/leo/Desktop/me 2/Scouting/Scouting_2025-2026/Test/tables/df_players_luiss.csv",
-    sep=';'
-)'''
 ROOT = Path(__file__).resolve().parents[1]   # da pages/ risale alla cartella principale
 
 # Percorso alla cartella data
 DATA = ROOT / "data"
 df_luiss_giocatori = pd.read_csv(DATA / "df_players_luiss.csv", sep=';')
+
+#df_luiss_giocatori=pd.read_csv("/Users/leo/Desktop/me 2/Scouting/Scouting_2025-2026/Test/tables/df_players_luiss.csv", sep=';')
 
 
 #app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
@@ -47,31 +23,31 @@ mygraph = dcc.Graph(figure={})
 
 # Setting the dropdown menu
 giocatori = dcc.Dropdown(
-    id='giocatori',
-    options=[{'label': i, 'value': i} for i in df_luiss_giocatori['Player'].unique()],
-    value=['Alessio', 'Fala'],
-    multi=True,
-    clearable=False
+id='giocatori',
+options=[{'label': i, 'value': i} for i in df_luiss_giocatori['Player'].unique()],
+value=['Alessio', 'Fala'],
+multi=True,
+clearable=False
 )
 home_away=dcc.Checklist(
-        id='checklist-items h/a',
-        options=[
-            {'label': 'Casa', 'value': 'H'},
-            {'label': 'Trasferta', 'value': 'A'}
-        ],
-        value=['H'],
-        labelStyle={'display': 'block'}
-    )
+id='checklist-items h/a',
+options=[
+{'label': 'Casa', 'value': 'H'},
+{'label': 'Trasferta', 'value': 'A'}
+],
+value=['H'],
+labelStyle={'display': 'block'}
+)
 
 win_lose=dcc.Checklist(
-        id='checklist-items w/l',
-        options=[
-            {'label': 'W', 'value': 'W'},
-            {'label': 'L', 'value': 'L'}
-        ],
-        value=['W'],
-        labelStyle={'display': 'block'}
-    )
+id='checklist-items w/l',
+options=[
+{'label': 'W', 'value': 'W'},
+{'label': 'L', 'value': 'L'}
+],
+value=['W'],
+labelStyle={'display': 'block'}
+)
 
 radio_item_statistica=dcc.RadioItems(
         id='radio-items statistiche',
@@ -96,41 +72,41 @@ radio_item_statistica=dcc.RadioItems(
     )
 
 layout = dbc.Container([
-    dbc.Row([
-        dbc.Col(html.Div([
-            html.H2('Analisi Per Giocatore'),
-            html.P("In questa sezione è possibile vedere uno a scelta tra diverse statistiche, "
-                   "filtrando per giocatore, partite vinte o perse e per partite in casa o in trasferta.")
-        ]), width=12)
-    ]),
+dbc.Row([
+dbc.Col(html.Div([
+html.H2('Analisi Per Giocatore'),
+html.P("In questa sezione è possibile vedere uno a scelta tra diverse statistiche, "
+"filtrando per giocatore, partite vinte o perse e per partite in casa o in trasferta.")
+]), width=12)
+]),
 
-    dbc.Row([
-        # Colonna sinistra - tutti i filtri uno sotto l'altro
-        dbc.Col([
-            html.Label('Casa/Trasferta'),
-            home_away,
-            html.Br(),
-            html.Label('W/L'),
-            win_lose,
-            html.Br(),
-            html.Label('Giocatori'),
-            giocatori,
-            html.Br(),
-            html.Label('Statistica'),
-            radio_item_statistica
-        ], width=3, style={'padding': '20px'}),
+dbc.Row([
+# Colonna sinistra - tutti i filtri uno sotto l'altro
+dbc.Col([
+html.Label('Casa/Trasferta'),
+home_away,
+html.Br(),
+html.Label('W/L'),
+win_lose,
+html.Br(),
+html.Label('Giocatori'),
+giocatori,
+html.Br(),
+html.Label('Statistica'),
+radio_item_statistica
+], width=3, style={'padding': '20px'}),
 
-        # Colonna destra - grafico
-        dbc.Col([
-            mygraph
-        ], width=9)
-    ]),
+# Colonna destra - grafico
+dbc.Col([
+mygraph
+], width=9)
+]),
 
-    dbc.Row([
-        dbc.Col([
-            html.Div(style={'height': '100px'})
-        ])
-    ], justify='center')
+dbc.Row([
+dbc.Col([
+html.Div(style={'height': '100px'})
+])
+], justify='center')
 ], fluid=True)
 
 
@@ -139,16 +115,16 @@ layout = dbc.Container([
 
 #@app.callback(
 @callback(        
-    Output(mygraph, 'figure'),
-    [Input('checklist-items h/a', 'value'),
-     Input('checklist-items w/l', 'value'),
-     Input('giocatori', 'value'),
-     Input('radio-items statistiche', 'value')]
+   Output(mygraph, 'figure'),
+   [Input('checklist-items h/a', 'value'),
+    Input('checklist-items w/l', 'value'),
+    Input('giocatori', 'value'),
+    Input('radio-items statistiche', 'value')]
 )
 
 def update_graph(home_away, win_lose, giocatore, statistica):
 
-    # Filter the dataframe according to user input
+# Filter the dataframe according to user input
     filtered_df = df_luiss_giocatori[(df_luiss_giocatori['Player'].isin(giocatore)) & (df_luiss_giocatori['HA'].isin(home_away))
                                      & (df_luiss_giocatori['WL'].isin(win_lose))]
     
@@ -170,7 +146,3 @@ def update_graph(home_away, win_lose, giocatore, statistica):
     )
 
     return fig
-
-
-
-
