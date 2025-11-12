7929c9383b800bc84968d2831530041822d5aec7
leoberrettoni <127773205+leoberrettoni@users.noreply.github.com>
2025-11-12 18:45:21 +0100

    Update page_4.py

--- stats/pages/page_4.py
+++ stats/pages/page_4.py
@@ -14,9 +14,22 @@ from dash.dash_table.Format import Format, Group, Scheme
 dash.register_page(__name__, path='/page-4', name='Pagina Attacco Luiss')
 
 
+
+
 # importing the DF
-df_match_luiss=pd.read_csv("/Users/leo/Desktop/me 2/Scouting/Scouting_2025-2026/Test/tables/df_match_luiss.csv", sep=';')
-df_luiss=pd.read_csv("/Users/leo/Desktop/me 2/Scouting/Scouting_2025-2026/Test/tables/df_teams_luiss.csv", sep=';')
+ROOT = Path(__file__).resolve().parents[1]   # da pages/ risale alla cartella principale
+
+# Percorso alla cartella data
+DATA = ROOT / "data"
+
+# Leggi i CSV
+df_match_luiss = pd.read_csv(DATA / "df_match_luiss.csv", sep=';')
+df_luiss = pd.read_csv(DATA / "df_teams_luiss.csv", sep=';')
+
+
+# importing the DF
+#df_match_luiss=pd.read_csv("/Users/leo/Desktop/me 2/Scouting/Scouting_2025-2026/Test/tables/df_match_luiss.csv", sep=';')
+#df_luiss=pd.read_csv("/Users/leo/Desktop/me 2/Scouting/Scouting_2025-2026/Test/tables/df_teams_luiss.csv", sep=';')
 df_luiss_merged = df_luiss.merge(df_match_luiss, on=['OPPONENT', 'DATA'], suffixes=('_opponent', '_match'), how='left')
 
 # dropping cols
